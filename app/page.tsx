f526fbfd359fe35c31a0862d44ac1c2cfa09d0b9
kengo2003 <saitokengo2003@gmail.com>
2025-05-25 22:29:33 +0900

    add: title

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import GanttChart from "@/components/gantt-chart";
 
 export default function Page() {
   return (
-    <div>
+    <main className="container mx-auto">
+      <div className="bg-slate-300 p-2">
+        <h1 className="text-2xl font-bold">プロジェクト管理ダッシュボード</h1>
+      </div>
       <GanttChart />
-    </div>
+    </main>
   );
 }
